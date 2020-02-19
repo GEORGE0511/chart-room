deeca9bb54902b8f48eb1ee2672dc2a0edb7e4b7
george <george0511703@gmail.com>
2020-02-20 01:46:49 +0800

    master

--- routes/qiaozhi.js
+++ routes/qiaozhi.js
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2020-02-20 01:33:13
- * @LastEditTime: 2020-02-20 01:40:10
+ * @LastEditTime: 2020-02-20 01:46:39
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \chart-room\routes\george.js
+ * @FilePath: \chart-room\routes\georges.js
  */
