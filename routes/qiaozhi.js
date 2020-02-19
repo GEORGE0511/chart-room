25f0c417e20119109b770a9ec24aa67b25eb7244
george <george0511703@gmail.com>
2020-02-20 01:41:42 +0800

    test

--- routes/qiaozhi.js
+++ routes/qiaozhi.js
@@ -1,9 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2020-02-20 01:33:13
- * @LastEditTime: 2020-02-20 01:36:48
+ * @LastEditTime: 2020-02-20 01:40:10
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: \chart-room\routes\qiaozhi.js
+ * @FilePath: \chart-room\routes\george.js
  */
-234
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-02-20 01:31:57
+ * @LastEditTime: 2020-02-20 01:40:51
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \chart-room\routes\users.js
+ */
 var express = require('express');
 var router = express.Router();
 
