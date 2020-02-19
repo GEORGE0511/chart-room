3dd47b56cb11a410295cf5aa0f612733cb8d2270
george <george0511703@gmail.com>
2020-02-20 01:59:28 +0800

    master

---
+++ george.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2020-02-20 01:59:15
+ * @LastEditTime: 2020-02-20 01:59:16
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \chart-room\george.js
+ */
